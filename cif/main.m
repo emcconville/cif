c8b4fb29a7b34d59ee25b84c5ef486ccdab4bf14
Eric McConville <emcconville@emcconville.com>
2015-06-16 20:25:50 -0500

    Initial Commit

---
+++ cif/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  cif
+//
+//  Created by Eric McConville on 6/16/15.
+//  Copyright (c) 2015 Eric McConville. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
