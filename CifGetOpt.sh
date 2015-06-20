5e2764ffc704dee66789422230896749133cd262
Eric McConville <emcconville@emcconville.com>
2015-06-20 10:18:00 -0500

    Reworked TAB script to have smaller footprint

--- CifGetOpt.sh
+++ CifGetOpt.sh
@@ -1,34 +1,27 @@
 #!/bin/sh
-
-#  CifGetOpt.sh
-#  cif
 #
-#  Created by Eric McConville on 6/18/15.
-#  Copyright (c) 2015 Eric McConville. All rights reserved.
+#  Copyright (c) 2015 Eric McConville.
+#  https://github.com/emcconville/cif
 
-__CifGetOpt__()
-{
-    local phrase
-    local __cif
-    COMPREPLY=()
-    phrase=${COMP_WORDS[COMP_CWORD]}
-    __cif=${COMP_WORDS[0]}
-
-    case "$phrase" in
-        CI*)
-            local words
-            words=$( ${__cif} list )
-            COMPREPLY=( $( compgen -W "$words" $phrase) )
-        ;;
-        -*)
-            local filter
-            local words
-            filter=${COMP_WORDS[1]}
-            words=$( ${__cif} list $filter | grep -e '^ -' | cut -d ' ' -f 2)
-            COMPREPLY=( $( compgen -W "$words" -- $phrase) )
-        ;;
-    esac
-    return 0;
+__CifGetOpt__() {
+ local C=${COMP_WORDS[0]}
+ local F
+ local P=${COMP_WORDS[COMP_CWORD]}
+ local R=()
+ local W
+ case "$P" in
+   CI*)
+    W=$(${C} list)
+    COMPREPLY=($(compgen -W "$W" $P))
+    ;;
+   -*)
+    F=${COMP_WORDS[1]}
+    W=$(${C} list $F|grep -e '^ -'|cut -d ' ' -f 2)
+    R=($( compgen -W "$W" -- $P))
+    ;;
+ esac
+ COMPREPLY=R
+ return 0;
 }
 
 complete -F __CifGetOpt__ -o default cif
---
+++ package.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#  package.sh
+#  cif
+#
+#  Created by Eric McConville on 6/19/15.
+#  Copyright (c) 2015 Eric McConville. All rights reserved.
